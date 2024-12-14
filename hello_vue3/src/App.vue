72b9460855601d81df68132f7791b719397df62e
Rombing <2609911870@qq.com>
2024-12-15 07:51:39 +0800

    test

---
+++ hello_vue3/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="app">
+        <h1>Hello Vue!</h1>
+        <Person></Person>
+    </div>
+</template>
+
+<script>
+    import Person from './components/Person.vue';
+
+    export default {
+        name: 'App',
+        components: {
+            Person
+        }
+    }
+</script>
+
+<style>
+    .app{
+        background-color:red;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
